3bdca32ec5ab3e86316d4bc2ab8f26556e21cabf
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-06-20 11:55:45 -0600

    build: add graqr dependency, set release version

--- src/main/java/com/graqr/PdpCommand.java
+++ src/main/java/com/graqr/PdpCommand.java
@@ -1,22 +1,19 @@
 package com.graqr;
 
 import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 @Command(name = "threshr-cli", description = "...",
         mixinStandardHelpOptions = true)
-public class ThreshrCliCommand implements Runnable {
+public class PdpCommand implements Runnable {
 
     @Option(names = {"-v", "--verbose"}, description = "...")
     boolean verbose;
 
     public static void main(String[] args) throws Exception {
-        PicocliRunner.run(ThreshrCliCommand.class, args);
+        PicocliRunner.run(PdpCommand.class, args);
     }
 
     public void run() {
